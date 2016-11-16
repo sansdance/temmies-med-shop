f6ae2a7811a6c4381458991c92506e8bf86546e7
Bendomar <bendomar.isla@gmail.com>
2016-11-16 23:26:47 +0800

    Mini Drugstore

---
+++ src/minidrugstore/MiniDrugStore.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minidrugstore;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MiniDrugStore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // this opens the login form
+        Login nlogin= new Login();
+        nlogin.setVisible(true);
+    }
+    
+}
